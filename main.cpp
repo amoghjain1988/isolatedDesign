285fe32861dd4b2e96e147efbae70b591e091763
Wb <amoghjain@gmail.com>
2021-08-22 14:54:47 -0400

    enum final.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,59 @@
 #include <iostream>
 
+#define ExecFunctio \
+X(started) \
+X(botted)\
+X(poweresent)\
+X(notuyetSet)\
+X(ReadtoSleep)
+
+// #define X(execlist) e_##execlist,   // to change events throughtout..
+
+
+#define X(execlist) execlist,   // can write another list usign ##
+enum Exec_Enum{
+    ExecFunctio
+};
+#undef X
+
+const char* convertEnumToString(Exec_Enum Obj)
+{
+       switch(Obj)
+        {
+            #define X(execlist)  \
+            case execlist: std::cout<<"\n Returning :"<<#execlist; return #execlist; break;
+            ExecFunctio
+            #undef X
+
+            default :
+            std::cout<<"\n Default Called";
+            break;
+        }
+}
+
 int main()
 {
-    std::cout<<"\n Size of Int :"<<sizeof(int);
-    std::cout<<"\n Size of Uint8_t"<<sizeof(uint8_t);
+   
+ 
+
+    #define X(execlist) std::cout<<"\n "<<#execlist;
+    ExecFunctio
+    #undef X
+    
+    Exec_Enum ExecObject = notuyetSet;
+    std::cout<<"\n "<<ExecObject;
+
+    for(int i = started; i <= ReadtoSleep; i++)
+    {
+        ExecObject= static_cast<Exec_Enum>(i);
+        
+        std::cout<<"\n Value of Enum" << i << " is " <<convertEnumToString(ExecObject);
+   
+    }
+
+
+
     return 0;
+std::cout<<"\n Exiting Program";
 }
+
