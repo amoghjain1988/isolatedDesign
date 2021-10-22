612abf2e4604c4645c9b7c095331e64bad1333e1
Wb <amoghjain@gmail.com>
2021-10-21 23:53:32 -0400

    first make complete!

---
+++ build/CMakeFiles/buildfile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buildfile.dir/main.cpp.o"
+  "buildfile.pdb"
+  "buildfile"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buildfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
