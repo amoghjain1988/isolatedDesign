c65cf4d339214ab741102d3db1821905ef97ca43
amoghjain1988 <amoghjain@hotmail.com>
2022-07-08 19:55:00 -0400

    FSM object inside class

--- BOOSTSML/main.cpp
+++ BOOSTSML/main.cpp
@@ -7,28 +7,47 @@
 // #include "AdminFSMObjects.cpp"
 
 #include "AdminTable.h"
+#include "EventsBridge.h"
 
-FSM_Admin::FSMAdmininstator<> AdminFSM{};
+class eventOBserver{
+
+FSM_Admin::FSMAdmininstator<> AdminFSM;
+
+public:
+            eventOBserver(){
+
+                FSM_Admin::FSMAdmininstator<> AdminFSM{};
+
+            };
+
+            ~eventOBserver(){};
+            
+            // void ExtractEvent() override{}
+
+            template<typename t>
+            void update( t myevname) {
+            eventOBserver::AdminFSM.ExternalEventProcessor(myevname);
+            }
+
+};
 
 
 
 
 
-template<typename t>
-void update( t myevname) {
- AdminFSM.ExternalEventProcessor(myevname);
-}
+
+
 
 
 int main() {
 
 
 std::cout<<"\n Program Start - Observing Events \n\n";
+eventOBserver myobs = eventOBserver();
 
+  myobs.update(FSM_Admin::powerUP());
 
-  update(FSM_Admin::powerUP());
-
-
+    
 std::cout<<"\n\n\n Program End \n\n";
 
 return 0;
