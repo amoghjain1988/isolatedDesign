b6ab3a9a0aef1d86cfcd11cd849e81778b5a5de5
amoghjain1988 <amoghjain@hotmail.com>
2022-07-08 01:00:22 -0400

    FSM wrapped up neatly in class

--- BOOSTSML/main.cpp
+++ BOOSTSML/main.cpp
@@ -9,13 +9,7 @@ int main() {
 std::cout<<"\n Program Start - Observe Events \n\n";
 
 FSM_Admin::FSMAdmininstator<> SM_Admin{};
-SM_Admin.process();
-  //SM_Admin.process_event(FSM_Admin::poweredUP{});
-  
-
-
-  //SM_Admin.process_event(FSM_Admin::CommsRequired{});
-
+SM_Admin.process(FSM_Admin::poweredUP{});
 
 return 0;
 }
