885cc033c7a3cdb2202224378952a9105f71da06
amoghjain1988 <amoghjain@hotmail.com>
2022-07-08 01:29:26 -0400

    imrpoved terminal readability

--- BOOSTSML/main.cpp
+++ BOOSTSML/main.cpp
@@ -2,7 +2,10 @@
 #include "AdminTable.h"
 
 
+class Timer{    // Sends
 
+
+};
 
 int main() {
 std::cout<<"\n Program Start - Observe Events \n\n";
@@ -11,6 +14,7 @@ FSM_Admin::FSMAdmininstator<> FSM_Admin{};
 
 // External Event Processor
 FSM_Admin.ExternalEventProcessor(FSM_Admin::poweredUP{});
+std::cout<<"\n\n\n Program End \n\n";
 
 return 0;
 }
