634b65857862473686205c4b9f530182a63ffc51
amoghjain1988 <amoghjain@hotmail.com>
2022-07-09 18:52:15 -0400

    removed comms header - seems NOT required..

--- BOOSTSML/comms.cpp
+++ BOOSTSML/comms.cpp
@@ -1,14 +1,17 @@
-#include "comms.h"
 #include<iostream>
  #include "AdminEvents.h"
 #include "sharedPtr.h"
 
+class Communications{
+public:
 
         template<typename t>
-        void Communications::doWork(std::shared_ptr<t>meraPointer)
+        void doWork(std::shared_ptr<t>meraPointer)
         {
                 meraPointer->ParseEvent(FSM_Admin::TimersUpdated{});
                 std::cout<<"\n Shared Ptr Count inside COmms Class : "<<meraPointer.use_count();
 
         }
 
+        
+};
