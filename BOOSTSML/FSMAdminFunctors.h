d4709f5247dc1fdf120002220a26e4e381ccd232
amoghjain1988 <amoghjain@hotmail.com>
2022-07-09 18:25:27 -0400

    removed unnecessary files and headers

--- BOOSTSML/FSMAdminFunctors.h
+++ BOOSTSML/FSMAdminFunctors.h
@@ -35,7 +35,7 @@ namespace FSM_Admin{
             auto hardwareEntry    = []() { std::cout<<"\n State Entry : Hardware Routine";    };
             auto CommsEntry       = []() { std::cout<<"\n State Entry : Comms Entry"; };
             auto ProvisionEntry   = []() { std::cout<<"\n State Entry : Provision Entry ";     };
-            inline auto PoweredUpEntry   = []() { std::cout<<"\n State Entry : PoweredUp Entry ";    myespNowObj.doWork(ReturnEvents<EventCallback>);    };
+            inline auto PoweredUpEntry   = []() { std::cout<<"\n State Entry : PoweredUp Entry ";    myespNowObj.doWork(std::move(ReturnEvents<EventCallback>)) ;    };
             auto routineEntry     = []() { std::cout<<"\n State Entry : routine Entry ";       };
               
               
--- BOOSTSML/sharedPtr.h
+++ BOOSTSML/sharedPtr.h
@@ -2,10 +2,8 @@
 #define sharedPtr
 #include <memory>
 
-#include <iostream>
 
 
-// class EventCallback;
 
 template<typename evclass>
 std::shared_ptr<evclass>ReturnEvents;         // provides access to Object in Main to
