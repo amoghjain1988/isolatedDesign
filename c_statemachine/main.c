0069f1dbedff7d3ba94f705e0e94ddfa5efcdcbc
Amogh Jain <amoghjain@hotmail.com>
2022-11-05 20:51:37 -0400

    Graph based state mchine readyyy!

--- c_statemachine/main.c
+++ c_statemachine/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+       #include <unistd.h>
 
+#include <stdlib.h>
 // Define the maximum number of vertices in the graph
 
 #define MACROSTR(k) #k
@@ -10,9 +13,11 @@
 #define TM_STATES_NUMBERS \
        X(NOT_YET_STARTED  ) \
        X(STATE_BOOTUP   ) \
+       X(STATE_WOKEUP   ) \
        X(STATE_WATER_ON   ) \
        X(STATE_WATER_OFF ) \
        X(STATE_ROUTINE  ) \
+       X(STATE_IDLE  ) \
        X(STATE_SLEEP  ) \
        X(STATE_ERROR  ) \
        X(STATE_NOT_UNUSED   )
@@ -49,6 +54,7 @@ static char *kSystemStatesStr[] =
        X(WAKEUP_SRC_POWERON  ) \
        X(WAKEUP_SRC_USERBTN  ) \
        X(WAKEUP_SRC_INLINE_SENSOR  ) \
+       X(UPDATE_HW_VALUES  ) \
        X(SYSTEM_STATE_WATER_ON  ) \
        X(SYSTEM_STATE_WATER_OFF  ) \
        X(SYSTEM_STATE_ROUTINE  ) \
@@ -188,24 +194,70 @@ void EventCycler()
 {
 
 }
+void printRelatedEdges(struct Node* NodeList, SYSTEM_STATE current_state,TM_EVENTS_t current_event )
+{
+    if(!NodeList)
+    {
+       // printf("'\n Invalid State. No Events Attached to this state yet..");
+        return;
+    }
+    while(NodeList!=NULL)
+    {
+
+        // if(!NodeList->next_state)
+        // {
+        //     printf("\n Event ")
+        //     return;
+
+        // }
+        int next_state                      = NodeList->next_state;
+        const char *str_next_state          = kSystemStatesStr[next_state];
+        TM_EVENTS_t transition_event        = NodeList->event;
+        const char *str_transition_event    = kEvntStr[transition_event];
+        
+        // printf("\n Start State[#%d]  = : %s", current_state, kSystemStatesStr[current_state]);
+        // printf(" ||\tTransition Event : %s", str_transition_event);
+        // printf(" ||\tNext State [#%d]  = : %s", next_state, str_next_state);
+
+        if(current_event == transition_event)
+        {
+            printf("\n Event [%s] Found in state : [%s]. Action can be taken..",kEvntStr[current_event], kSystemStatesStr[current_state]);
+            return;
+        }
+        //NodeList->event_found_action();
+
+        NodeList = NodeList->next;
+
+    }
+     printf("\n Ignoring Event [%s]  as it is not Associated with Current State [%s]... ",kEvntStr[current_event], kSystemStatesStr[current_state]);
+}
+
+int random_generator(int min, int max){
+    
+    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
 
 // Weighted Directed graph implementation in C
 int main(void)
-{
+{   
+    srand(time(NULL));
+    
     // input array containing edges of the graph (as per the above diagram)
     // (x, y, w) tuple represents an edge from x to y having weight `w`
     struct Edge edges[] =
     {
         {NOT_YET_STARTED,   BOOTUP_CHECK_START,             BootUpAction,  STATE_BOOTUP    },          // Stay in Routine until Finished is Released..        
         {STATE_BOOTUP,      BOOTUP_CHECK_START,             BootUpAction,  STATE_BOOTUP    },          // Stay in Routine until Finished is Released..
-        {STATE_BOOTUP,      BOOTUP_CHECK_FINISHED,          BootUpAction,  STATE_ROUTINE   }, 
+        {STATE_BOOTUP,      BOOTUP_CHECK_FINISHED,          BootUpAction,  STATE_IDLE       }, 
         {STATE_WATER_ON,    SYSTEM_STATE_WATER_ON,          BootUpAction,  STATE_WATER_ON  },          // Stay in Water On State if Water is Found..
         {STATE_WATER_ON,    SYSTEM_STATE_WATER_OFF,         BootUpAction,  STATE_WATER_OFF },          // Water On Transitions to Water Off..
         {STATE_WATER_ON,    USER_BTN_LONG_PRESS,            BootUpAction,  STATE_WATER_OFF },          // User Butn during Water on Shuts Motor Down..
         {STATE_WATER_OFF,   SYSTEM_STATE_WATER_OFF,         BootUpAction,  STATE_ROUTINE   }, 
         {STATE_ROUTINE,     SYSTEM_STATE_LIGHT_SLEEP_INIT,  BootUpAction,  STATE_SLEEP     }, 
         {STATE_ROUTINE,     SYSTEM_STATE_DEEP_SLEEP_INIT,   BootUpAction,  STATE_SLEEP     }, 
-        {STATE_ROUTINE,     EVENT_ERROR,                    BootUpAction,  STATE_ERROR     }
+        {STATE_ROUTINE,     EVENT_ERROR,                    BootUpAction,  STATE_ERROR     },
+        {STATE_IDLE,        UPDATE_HW_VALUES,               BootUpAction,  STATE_ROUTINE   }, 
+        {STATE_IDLE,        SYSTEM_STATE_LIGHT_SLEEP_INIT,  BootUpAction,  STATE_SLEEP     }
 
     };
  
@@ -218,29 +270,40 @@ int main(void)
  
 
     // Function to print adjacency list representation of a graph
-    printStateTransitionTable(graph);
+    //printStateTransitionTable(graph);
 
-    SYSTEM_STATE current_state = STATE_WATER_ON;
 
-    struct Node* NodeList = graph->TransitionTable[current_state];
+    int x = 0;
 
-    while(NodeList)
+    while(x<100)
     {
-        int next_state                      = NodeList->next_state;
-        const char *str_next_state          = kSystemStatesStr[next_state];
-        TM_EVENTS_t transition_event        = NodeList->event;
-        const char *str_transition_event    = kEvntStr[transition_event];
-        
-        printf("\n Start State[#%d]  = : %s", current_state, kSystemStatesStr[current_state]);
-        printf(" ||\tTransition Event : %s", str_transition_event);
-        printf(" ||\tNext State [#%d]  = : %s", next_state, str_next_state);
+        printf("\n ~~~~~~~~~~~~~~~~ x=[%d]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", x);
 
-        NodeList->event_found_action();
+        struct Node* NodeList = (struct Node*)malloc(sizeof( struct Node));
 
-        NodeList = NodeList->next;
+        x++;
+
+        SYSTEM_STATE random_state = random_generator(NOT_YET_STARTED, STATE_NOT_UNUSED-1);
+        TM_EVENTS_t random_event = random_generator(NO_EVENTS, EVENT_COUNT-1);
+
+        // SYSTEM_STATE random_state = STATE_WATER_ON;
+        // TM_EVENTS_t random_event = WAKEUP_SRC_USERBTN;
+
+        const char* random_state_str = kSystemStatesStr[random_state];
+        const char* random_event_str = kEvntStr[random_event];
+
+        printf("\n Random State : [%s], Random Event : [%s]",random_state_str,  random_event_str);
+
+        NodeList = graph->TransitionTable[random_state];
+
+        printRelatedEdges(NodeList,random_state,random_event);
+
+        free(NodeList);
+        // sleep(1);
+        // printf("\n ~~~~~~~~~~~~~~~~~~~~~~END ~~~~~~~~~~~~~~~~~~~~~~~~~");
 
     }
-     printf("\n");
+
 
 
     return 0;
